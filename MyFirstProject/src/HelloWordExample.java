93ba8015e38c1bc1ce7189dd2244ef59b26a7473
simongreen-op <simongreen3@myopenpolytechnic.ac.nz>
2018-07-03 22:22:00 +1200

    second comment

--- MyFirstProject/src/HelloWordExample.java
+++ MyFirstProject/src/HelloWordExample.java
@@ -2,7 +2,7 @@
 public class HelloWordExample {
 
 	public static void main(String[] args) {
-		// Output Hello Word to the user
+		// Print something to the user
 		System.out.println("Hello World");
 
 	}
